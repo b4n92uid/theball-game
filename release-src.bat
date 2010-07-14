d305b0e2ee5fb024bd2adea733fc18aed5bef194
Beldjouhri Abdelghani <b4n92uid@gmail.com>
2010-07-14 19:51:27 +0200

    Pris en charge des ressource par hg

--- release-src.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set OUTPUT=theball-1.0.%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%.7z
-title Making %OUTPUT%
-if exist "%OUTPUT%" del "%OUTPUT%"
-7z a "%OUTPUT%" * -mtc=on -mx=9 -x!versions -x!sourcesold -x!build -xr!doc/html -xr!*.lnk -xr!*.bat -xr!*.tdl -xr!.hg*
-echo End of process
-pause > nul
