bfade212fe294c8c94bfc5a3e77337f3fcc9d319
b4n92uid <b4n92uid@localhost>
2011-04-05 18:00:49 +0100

    Imp: MaterialManager contact process

--- sources/MaterialManager.cpp
+++ sources/MaterialManager.cpp
@@ -20,215 +20,6 @@ using namespace std;
 using namespace tbe;
 using namespace tbe::scene;
 
-template<typename T> T GetUserData(const NewtonBody* body)
-{
-    NewtonNode* nn = static_cast<NewtonNode*>(NewtonBodyGetUserData(body));
-    return dynamic_cast<T>(nn);
-}
-
-template<typename T> T GetParentUserData(const NewtonBody* body)
-{
-    NewtonNode* nn = static_cast<NewtonNode*>(NewtonBodyGetUserData(body));
-    return dynamic_cast<T>(nn->GetParent());
-}
-
-void PlayerOnStaticContactsProcess(const NewtonJoint* contact, dFloat timestep, int threadIndex)
-{
-    NewtonBody* body0 = NewtonJointGetBody0(contact);
-    NewtonBody* body1 = NewtonJointGetBody1(contact);
-
-    int group0 = NewtonBodyGetMaterialGroupID(body0);
-    int group1 = NewtonBodyGetMaterialGroupID(body1);
-
-    if(group0 == group1)
-        return;
-
-    NewtonWorld* nWorld = NewtonBodyGetWorld(body0);
-    GameManager* gameManager = static_cast<GameManager*>(NewtonMaterialGetUserData(nWorld, group0, group1));
-    int playerGroup = gameManager->manager.material->GetPlayersGroupe();
-
-    Player* player = NULL;
-    NewtonNode* obj = NULL;
-
-    if(group0 == playerGroup)
-    {
-        player = GetParentUserData<Player*>(body0);
-        obj = GetParentUserData<NewtonNode*>(body1);
-    }
-
-    else if(group1 == playerGroup)
-    {
-        player = GetParentUserData<Player*>(body1);
-        obj = GetParentUserData<NewtonNode*>(body0);
-    }
-}
-
-void PlayerOnDynamicContactsProcess(const NewtonJoint* contact, dFloat, int)
-{
-    NewtonBody* body0 = NewtonJointGetBody0(contact);
-    NewtonBody* body1 = NewtonJointGetBody1(contact);
-
-    int group0 = NewtonBodyGetMaterialGroupID(body0);
-    int group1 = NewtonBodyGetMaterialGroupID(body1);
-
-    if(group0 == group1)
-        return;
-
-    NewtonWorld* nWorld = NewtonBodyGetWorld(body0);
-    GameManager* gameManager = static_cast<GameManager*>(NewtonMaterialGetUserData(nWorld, group0, group1));
-    int playerGroup = gameManager->manager.material->GetPlayersGroupe();
-
-    Player* player = NULL;
-    DynamicObject* obj = NULL;
-
-    if(group0 == playerGroup)
-    {
-        obj = GetParentUserData<DynamicObject*>(body1);
-        player = GetParentUserData<Player*>(body0);
-    }
-
-    else if(group1 == playerGroup)
-    {
-        obj = GetParentUserData<DynamicObject*>(body0);
-        player = GetParentUserData<Player*>(body1);
-    }
-
-    obj->InteractWith(player);
-}
-
-int PlayerOnItemsAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int)
-{
-    int group0 = NewtonBodyGetMaterialGroupID(body0);
-    int group1 = NewtonBodyGetMaterialGroupID(body1);
-
-    if(group0 == group1)
-        return 0;
-
-    Player* player = NULL;
-    Item* item = NULL;
-
-    NewtonWorld* nWorld = NewtonBodyGetWorld(body0);
-    GameManager* gameManager = static_cast<GameManager*>(NewtonMaterialGetUserData(nWorld, group0, group1));
-    int itemGroup = gameManager->manager.material->GetItemGroupe();
-
-    if(group0 == itemGroup)
-    {
-        item = GetParentUserData<Item*>(body0);
-        player = GetParentUserData<Player*>(body1);
-    }
-
-    else if(group1 == itemGroup)
-    {
-        item = GetParentUserData<Item*>(body1);
-        player = GetParentUserData<Player*>(body0);
-    }
-
-    player->AttachItem(item);
-
-    return 0;
-}
-
-void BulletOnMapContactsProcess(const NewtonJoint* contact, dFloat, int)
-{
-    NewtonBody* body0 = NewtonJointGetBody0(contact);
-    NewtonBody* body1 = NewtonJointGetBody1(contact);
-
-    int group0 = NewtonBodyGetMaterialGroupID(body0);
-    int group1 = NewtonBodyGetMaterialGroupID(body1);
-
-    if(group0 == group1)
-        return;
-
-    NewtonWorld* nWorld = NewtonBodyGetWorld(body0);
-    GameManager* gameManager = static_cast<GameManager*>(NewtonMaterialGetUserData(nWorld, group0, group1));
-    int weaponGroup = gameManager->manager.material->GetWeaponsGroupe();
-
-    Bullet* bullet = NULL;
-
-    if(group0 == weaponGroup)
-        bullet = GetUserData<Bullet*>(body0);
-
-    else if(group1 == weaponGroup)
-        bullet = GetUserData<Bullet*>(body1);
-
-    bullet->SetLife(0);
-}
-
-int BulletOnPlayerAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int)
-{
-    static tbe::ticks::Clock cheerClock;
-    static int cheerCount = 0;
-
-    int group0 = NewtonBodyGetMaterialGroupID(body0);
-    int group1 = NewtonBodyGetMaterialGroupID(body1);
-
-    if(group0 == group1)
-        return 0;
-
-    Player* striker = NULL;
-    Player* striked = NULL;
-    Bullet* bullet = NULL;
-
-    NewtonWorld* nWorld = NewtonBodyGetWorld(body0);
-    GameManager* gameManager = static_cast<GameManager*>(NewtonMaterialGetUserData(nWorld, group0, group1));
-    int weaponGroup = gameManager->manager.material->GetWeaponsGroupe();
-
-    if(group0 == weaponGroup)
-    {
-        striked = GetParentUserData<Player*>(body1);
-        bullet = GetUserData<Bullet*>(body0);
-    }
-
-    else if(group1 == weaponGroup)
-    {
-        striked = GetParentUserData<Player*>(body0);
-        bullet = GetUserData<Bullet*>(body1);
-    }
-
-    striker = bullet->GetWeapon()->GetShooter();
-    PlayManager* playManager = striker->GetPlayManager();
-
-    if(striked == striker)
-        return 0;
-
-    striked->TakeDammage(bullet);
-
-    bullet->SetLife(0);
-
-    if(striker == playManager->GetUserPlayer() && striked->IsKilled())
-    {
-        cheerCount++;
-
-        if(cheerCount >= 2)
-        {
-            if(!cheerClock.IsEsplanedTime(4000))
-                switch(cheerCount)
-                {
-                    case 2: playManager->Log("Double kill");
-                        break;
-                    case 3: playManager->Log("Triple kill");
-                        break;
-                    case 4: playManager->Log("Carnage");
-                        break;
-                    case 5: playManager->Log("The One");
-                        break;
-                    case 6: playManager->Log("War Warrior");
-                        break;
-                    case 7: playManager->Log("God Of War !!!");
-                        break;
-                    default: playManager->Log("...");
-                        break;
-                }
-            else
-                cheerCount = 0;
-
-            cheerClock.SnapShoot();
-        }
-    }
-
-    return 0;
-}
-
 MaterialManager::MaterialManager(GameManager * gameManager)
 {
     m_gameManager = gameManager;
@@ -244,26 +35,25 @@ MaterialManager::MaterialManager(GameManager * gameManager)
     m_dynamicGroupe = NewtonMaterialCreateGroupID(m_world);
     m_ghostGroupe = NewtonMaterialCreateGroupID(m_world);
 
-    NewtonMaterialSetCollisionCallback(m_world, m_playersGroupe, m_dynamicGroupe, m_gameManager, 0, PlayerOnDynamicContactsProcess);
-    //    NewtonMaterialSetCollisionCallback(m_world, m_playersGroupe, m_staticGroupe, 0, 0, PlayerOnStaticContactsProcess);
+    NewtonMaterialSetCollisionCallback(m_world, m_playersGroupe, m_dynamicGroupe, this, NULL, PlayerOnDynamicContactsProcess);
+    // NewtonMaterialSetCollisionCallback(m_world, m_playersGroupe, m_staticGroupe, this, NULL, PlayerOnStaticContactsProcess);
 
-    NewtonMaterialSetCollisionCallback(m_world, m_playersGroupe, m_itemGroupe, m_gameManager, PlayerOnItemsAABBOverlape, 0);
+    NewtonMaterialSetCollisionCallback(m_world, m_playersGroupe, m_itemGroupe, this, PlayerOnItemsAABBOverlape, NULL);
 
-    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_dynamicGroupe, m_gameManager, 0, BulletOnMapContactsProcess);
-    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_staticGroupe, m_gameManager, 0, BulletOnMapContactsProcess);
-    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_itemGroupe, m_gameManager, 0, BulletOnMapContactsProcess);
-    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_bulletGroupe, m_gameManager, BulletOnPlayerAABBOverlape, 0);
-    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_playersGroupe, m_gameManager, BulletOnPlayerAABBOverlape, 0);
-
-    NewtonMaterialSetDefaultFriction(m_world, m_playersGroupe, m_staticGroupe, worldSettings.staticFriction, worldSettings.keniticFriction);
+    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_dynamicGroupe, this, NULL, BulletOnMapContactsProcess);
+    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_staticGroupe, this, NULL, BulletOnMapContactsProcess);
+    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_itemGroupe, this, NULL, BulletOnMapContactsProcess);
+    NewtonMaterialSetCollisionCallback(m_world, m_bulletGroupe, m_playersGroupe, this, BulletOnPlayerAABBOverlape, NULL);
 
     NewtonMaterialSetDefaultCollidable(m_world, m_ghostGroupe, m_bulletGroupe, false);
     NewtonMaterialSetDefaultCollidable(m_world, m_ghostGroupe, m_playersGroupe, false);
     NewtonMaterialSetDefaultCollidable(m_world, m_ghostGroupe, m_itemGroupe, false);
     NewtonMaterialSetDefaultCollidable(m_world, m_ghostGroupe, m_staticGroupe, false);
     NewtonMaterialSetDefaultCollidable(m_world, m_ghostGroupe, m_dynamicGroupe, false);
+    // NewtonMaterialSetDefaultCollidable(m_world, m_bulletGroupe, m_itemGroupe, false);
 
-    //    NewtonMaterialSetDefaultCollidable(m_world, m_bulletGroupe, m_itemGroupe, false);
+
+    NewtonMaterialSetDefaultFriction(m_world, m_playersGroupe, m_staticGroupe, worldSettings.staticFriction, worldSettings.keniticFriction);
 }
 
 MaterialManager::~MaterialManager()
@@ -343,4 +133,181 @@ int MaterialManager::GetDynamicGroupe() const
 int MaterialManager::GetStaticGroupe() const
 {
     return m_staticGroupe;
+}
+
+void MaterialManager::mPlayerOnStaticContactsProcess(const NewtonJoint* contact, dFloat, int)
+{
+    NewtonBody* body0 = NewtonJointGetBody0(contact);
+    NewtonBody* body1 = NewtonJointGetBody1(contact);
+
+    int group0 = NewtonBodyGetMaterialGroupID(body0);
+    int group1 = NewtonBodyGetMaterialGroupID(body1);
+
+    if(group0 == group1 || group0 == m_ghostGroupe || group1 == m_ghostGroupe)
+        return;
+
+    Player* player = NULL;
+    NewtonNode* obj = NULL;
+
+    if(group0 == m_playersGroupe)
+    {
+        player = GetParentUserData<Player*>(body0);
+        obj = GetParentUserData<NewtonNode*>(body1);
+    }
+
+    else if(group1 == m_playersGroupe)
+    {
+        player = GetParentUserData<Player*>(body1);
+        obj = GetParentUserData<NewtonNode*>(body0);
+    }
+}
+
+void MaterialManager::mPlayerOnDynamicContactsProcess(const NewtonJoint* contact, dFloat, int)
+{
+    NewtonBody* body0 = NewtonJointGetBody0(contact);
+    NewtonBody* body1 = NewtonJointGetBody1(contact);
+
+    int group0 = NewtonBodyGetMaterialGroupID(body0);
+    int group1 = NewtonBodyGetMaterialGroupID(body1);
+
+    if(group0 == group1 || group0 == m_ghostGroupe || group1 == m_ghostGroupe)
+        return;
+
+    Player* player = NULL;
+    DynamicObject* obj = NULL;
+
+    if(group0 == m_playersGroupe)
+    {
+        obj = GetParentUserData<DynamicObject*>(body1);
+        player = GetParentUserData<Player*>(body0);
+    }
+
+    else if(group1 == m_playersGroupe)
+    {
+        obj = GetParentUserData<DynamicObject*>(body0);
+        player = GetParentUserData<Player*>(body1);
+    }
+
+    obj->InteractWith(player);
+}
+
+int MaterialManager::mPlayerOnItemsAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int)
+{
+    int group0 = NewtonBodyGetMaterialGroupID(body0);
+    int group1 = NewtonBodyGetMaterialGroupID(body1);
+
+    if(group0 == group1 || group0 == m_ghostGroupe || group1 == m_ghostGroupe)
+        return 0;
+
+    Player* player = NULL;
+    Item* item = NULL;
+
+    if(group0 == m_itemGroupe)
+    {
+        item = GetParentUserData<Item*>(body0);
+        player = GetParentUserData<Player*>(body1);
+    }
+
+    else if(group1 == m_itemGroupe)
+    {
+        item = GetParentUserData<Item*>(body1);
+        player = GetParentUserData<Player*>(body0);
+    }
+
+    player->AttachItem(item);
+
+    return 0;
+}
+
+void MaterialManager::mBulletOnMapContactsProcess(const NewtonJoint* contact, dFloat, int)
+{
+    NewtonBody* body0 = NewtonJointGetBody0(contact);
+    NewtonBody* body1 = NewtonJointGetBody1(contact);
+
+    int group0 = NewtonBodyGetMaterialGroupID(body0);
+    int group1 = NewtonBodyGetMaterialGroupID(body1);
+
+    if(group0 == group1 || group0 == m_ghostGroupe || group1 == m_ghostGroupe)
+        return;
+
+    Bullet* bullet = NULL;
+
+    if(group0 == m_bulletGroupe)
+        bullet = GetUserData<Bullet*>(body0);
+
+    else if(group1 == m_bulletGroupe)
+        bullet = GetUserData<Bullet*>(body1);
+
+    bullet->SetLife(0);
+}
+
+int MaterialManager::mBulletOnPlayerAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int)
+{
+    static tbe::ticks::Clock cheerClock;
+    static int cheerCount = 0;
+
+    int group0 = NewtonBodyGetMaterialGroupID(body0);
+    int group1 = NewtonBodyGetMaterialGroupID(body1);
+
+    if(group0 == group1 || group0 == m_ghostGroupe || group1 == m_ghostGroupe)
+        return 0;
+
+    Player* striker = NULL;
+    Player* striked = NULL;
+    Bullet* bullet = NULL;
+
+    if(group0 == m_bulletGroupe)
+    {
+        striked = GetParentUserData<Player*>(body1);
+        bullet = GetUserData<Bullet*>(body0);
+    }
+
+    else if(group1 == m_bulletGroupe)
+    {
+        striked = GetParentUserData<Player*>(body0);
+        bullet = GetUserData<Bullet*>(body1);
+    }
+
+    striker = bullet->GetWeapon()->GetShooter();
+    PlayManager* playManager = striker->GetPlayManager();
+
+    if(striked == striker)
+        return 0;
+
+    striked->TakeDammage(bullet);
+
+    bullet->SetLife(0);
+
+    if(striker == playManager->GetUserPlayer() && striked->IsKilled())
+    {
+        cheerCount++;
+
+        if(cheerCount >= 2)
+        {
+            if(!cheerClock.IsEsplanedTime(4000))
+                switch(cheerCount)
+                {
+                    case 2: playManager->Log("Double kill");
+                        break;
+                    case 3: playManager->Log("Triple kill");
+                        break;
+                    case 4: playManager->Log("Carnage");
+                        break;
+                    case 5: playManager->Log("The One");
+                        break;
+                    case 6: playManager->Log("War Warrior");
+                        break;
+                    case 7: playManager->Log("God Of War !!!");
+                        break;
+                    default: playManager->Log("...");
+                        break;
+                }
+            else
+                cheerCount = 0;
+
+            cheerClock.SnapShoot();
+        }
+    }
+
+    return 0;
 }
--- sources/MaterialManager.h
+++ sources/MaterialManager.h
@@ -38,6 +38,83 @@ public:
     int GetStaticGroupe() const;
 
 protected:
+
+    template<typename T> static T GetUserData(const NewtonBody* body)
+    {
+        tbe::scene::NewtonNode* nn = static_cast<tbe::scene::NewtonNode*>(NewtonBodyGetUserData(body));
+        return dynamic_cast<T>(nn);
+    }
+
+    template<typename T> static T GetParentUserData(const NewtonBody* body)
+    {
+        tbe::scene::NewtonNode* nn = static_cast<tbe::scene::NewtonNode*>(NewtonBodyGetUserData(body));
+        return dynamic_cast<T>(nn->GetParent());
+    }
+
+    static MaterialManager* getMaterialManager(const NewtonJoint* contact)
+    {
+        return getMaterialManager(NewtonJointGetBody0(contact),
+                                  NewtonJointGetBody1(contact));
+    }
+
+    static MaterialManager* getMaterialManager(const NewtonBody* body0, const NewtonBody* body1)
+    {
+        return (MaterialManager*)NewtonMaterialGetUserData(NewtonBodyGetWorld(body0),
+                                                           NewtonBodyGetMaterialGroupID(body0),
+                                                           NewtonBodyGetMaterialGroupID(body1));
+    }
+
+    void mBulletOnMapContactsProcess(const NewtonJoint* contact, dFloat, int);
+    int mBulletOnPlayerAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int);
+    int mPlayerOnItemsAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int);
+    void mPlayerOnDynamicContactsProcess(const NewtonJoint* contact, dFloat, int);
+    void mPlayerOnStaticContactsProcess(const NewtonJoint* contact, dFloat timestep, int threadIndex);
+
+    static void BulletOnMapContactsProcess(const NewtonJoint* contact, dFloat f, int i)
+    {
+        MaterialManager* matmanager = getMaterialManager(contact);
+
+        if(matmanager)
+            matmanager->mBulletOnMapContactsProcess(contact, f, i);
+    }
+
+    static int BulletOnPlayerAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int i)
+    {
+        MaterialManager* matmanager = getMaterialManager(body0, body1);
+
+        if(matmanager)
+            return matmanager->mBulletOnPlayerAABBOverlape(material, body0, body1, i);
+        else
+            return 0;
+    }
+
+    static int PlayerOnItemsAABBOverlape(const NewtonMaterial* material, const NewtonBody* body0, const NewtonBody* body1, int i)
+    {
+        MaterialManager* matmanager = getMaterialManager(body0, body1);
+
+        if(matmanager)
+            return matmanager->mPlayerOnItemsAABBOverlape(material, body0, body1, i);
+        else
+            return 0;
+    }
+
+    static void PlayerOnDynamicContactsProcess(const NewtonJoint* contact, dFloat timestep, int threadIndex)
+    {
+        MaterialManager* matmanager = getMaterialManager(contact);
+
+        if(matmanager)
+            matmanager->mPlayerOnDynamicContactsProcess(contact, timestep, threadIndex);
+    }
+
+    static void PlayerOnStaticContactsProcess(const NewtonJoint* contact, dFloat timestep, int threadIndex)
+    {
+        MaterialManager* matmanager = getMaterialManager(contact);
+
+        if(matmanager)
+            matmanager->mPlayerOnStaticContactsProcess(contact, timestep, threadIndex);
+    }
+
+protected:
     NewtonWorld* m_world;
     GameManager* m_gameManager;
     int m_staticGroupe;
